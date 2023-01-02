75e3d4688f52dcc4c407a0cddcd5b96c176fae16
Khoa Mai <mdk194@gmail.com>
2023-01-03 00:26:26 +0200

    vim-easy-align: change visual mode key to space

--- lua/plugins/vim-easy-align.lua
+++ lua/plugins/vim-easy-align.lua
@@ -1,6 +1,6 @@
 local M = { 'junegunn/vim-easy-align' }
 
-vim.api.nvim_set_keymap('v', '=', '<Plug>(EasyAlign)', { noremap = false })
+vim.api.nvim_set_keymap('v', '<space>', '<Plug>(EasyAlign)', { noremap = false })
 vim.api.nvim_set_keymap('n', 'g=', '<Plug>(EasyAlign)', { noremap = false })
 
 return M
