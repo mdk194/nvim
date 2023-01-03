bde53d4dfe59e0aa8eeb0559237d96e4d8c521a6
Khoa Mai <mdk194@gmail.com>
2023-01-03 20:34:19 +0200

    add peek.nvim

---
+++ lua/plugins/peek.lua
@@ -0,0 +1,19 @@
+return {
+  'toppair/peek.nvim',
+  build = 'deno task --quiet build:fast',
+  keys = {
+    {
+      "<leader>op",
+      function()
+        local peek = require("peek")
+        if peek.is_open() then
+          peek.close()
+        else
+          peek.open()
+        end
+      end,
+      desc = "Peek (Markdown Preview)",
+    },
+  },
+  config = { theme = "dark" },
+}
