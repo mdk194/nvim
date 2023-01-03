564d2f63a052f10daa976181fdff8de5aa8948e1
Khoa Mai <mdk194@gmail.com>
2023-01-03 11:49:47 +0200

    null-ls: add command to enable/disable

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -9,6 +9,8 @@ local M = {
 
 function M.config()
   require('plugins.lsp.null-ls')
+  vim.cmd([[ command! DisableNullLs execute 'lua require("null-ls").disable({})' ]])
+  vim.cmd([[ command! EnableNullLs execute 'lua require("null-ls").enable({})' ]])
 end
 
 return M
