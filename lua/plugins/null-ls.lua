2c60ca62e1fe82bc10e64eb376676c5523fdca91
Khoa Mai <mdk194@gmail.com>
2023-01-03 13:04:24 +0200

    null-ls: refactor configuration
    - move configuration to the lazy plugin config function
    - use same lsp custom_on_attach function
    - manual trigger format instead of auto format on save

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -8,7 +8,27 @@ local M = {
 }
 
 function M.config()
-  require('plugins.lsp.null-ls')
+  local nls = require('null-ls')
+  nls.setup({
+    sources = {
+      -- nls.builtins.formatting.stylua.with({ extra_args = { "--indent-type", "Spaces", "--indent-width", "2", "column_width", "300" } }),
+      nls.builtins.diagnostics.eslint_d,
+      nls.builtins.formatting.prettier.with({
+        extra_args = { "--single-quote", "false" },
+      }),
+      nls.builtins.formatting.goimports,
+      nls.builtins.formatting.gofumpt,
+      nls.builtins.code_actions.shellcheck,
+      nls.builtins.diagnostics.vale,
+      -- nls.builtins.diagnostics.commitlint,
+      -- nls.builtins.diagnostics.golangci_lint,
+      -- nls.builtins.diagnostics.ktlint,
+    },
+    on_attach = function(client, bufnr)
+      require('plugins.lsp.utils').custom_lsp_attach(client, bufnr)
+    end,
+  })
+
   vim.cmd([[ command! DisableNullLs execute 'lua require("null-ls").disable({})' ]])
   vim.cmd([[ command! EnableNullLs execute 'lua require("null-ls").enable({})' ]])
 end
