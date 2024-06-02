587ad52eeacc767620a84848b7005fb782097dd1
Khoa Mai <mdk194@gmail.com>
2024-06-02 23:34:07 +0300

    switch to mini-surround

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,32 +1,47 @@
 local M = {
-  'kylechui/nvim-surround',
+  'echasnovski/mini.surround',
   version = '*',
-  event = 'VeryLazy',
 }
 
 function M.config()
-  require("nvim-surround").setup({
-    aliases = {
-      ["a"] = ">",
-      ["b"] = ")",
-      ["B"] = "}",
-      ["r"] = "]",
-      ["q"] = { '"', "'", "`" },
-      ["s"] = { "}", "]", ")", ">", '"', "'", "`" },
+  require("mini.surround").setup({
+    -- Add custom surroundings to be used on top of builtin ones. For more
+    -- information with examples, see `:h MiniSurround.config`.
+    custom_surroundings = nil,
+
+    -- Duration (in ms) of highlight when calling `MiniSurround.highlight()`
+    highlight_duration = 500,
+
+    -- Module mappings. Use `''` (empty string) to disable one.
+    mappings = {
+      add = 'sa', -- Add surrounding in Normal and Visual modes
+      delete = 'sd', -- Delete surrounding
+      find = 'sf', -- Find surrounding (to the right)
+      find_left = 'sF', -- Find surrounding (to the left)
+      highlight = 'sh', -- Highlight surrounding
+      replace = 'sr', -- Replace surrounding
+      update_n_lines = 'sn', -- Update `n_lines`
+
+      suffix_last = 'l', -- Suffix to search with "prev" method
+      suffix_next = 'n', -- Suffix to search with "next" method
     },
-    keymaps = {
-      -- key end in _line -> new line, end in _cur -> current line
-      insert = "<c-g>s",
-      insert_line = "<C-g>S",
-      normal = "ys",
-      normal_cur = "yss",
-      normal_line = "yS",
-      normal_cur_line = "ySS",
-      visual = "S",
-      visual_line = "gS",
-      delete = "ds",
-      change = "cs",
-    },
+
+    -- Number of lines within which surrounding is searched
+    n_lines = 20,
+
+    -- Whether to respect selection type:
+    -- - Place surroundings on separate lines in linewise mode.
+    -- - Place surroundings on each line in blockwise mode.
+    respect_selection_type = false,
+
+    -- How to search for surrounding (first inside current line, then inside
+    -- neighborhood). One of 'cover', 'cover_or_next', 'cover_or_prev',
+    -- 'cover_or_nearest', 'next', 'prev', 'nearest'. For more details,
+    -- see `:h MiniSurround.config`.
+    search_method = 'cover',
+
+    -- Whether to disable showing non-error feedback
+    silent = false,
   })
 end
 
