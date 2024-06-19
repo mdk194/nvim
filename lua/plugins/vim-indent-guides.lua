c06053f53cf18e6ae9b32503628cad89ee7cb935
Khoa Mai <mdk194@gmail.com>
2024-06-19 19:58:54 +0300

    switch back to vim-indent-guides.lua for listchars

---
+++ lua/plugins/vim-indent-guides.lua
@@ -0,0 +1,10 @@
+vim.g.indent_guides_enable_on_vim_startup = 1
+vim.g.indent_guides_auto_colors = 0
+vim.g.indent_guides_exclude_buftype = 1
+--vim.g.indent_guides_exclude_filetypes = ['help', 'nerdtree']
+
+local M = {
+  'nathanaelkane/vim-indent-guides',
+}
+
+return M
