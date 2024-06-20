ce8fd7be21f5cb14872029a1db2141f9f8b3dfd4
Khoa Mai <mdk194@gmail.com>
2024-06-21 01:15:56 +0300

    override global print to notify

--- lua/plugins/nvim-notify.lua
+++ lua/plugins/nvim-notify.lua
@@ -24,6 +24,17 @@ function M.config()
       notify(msg, level, opts)
     end
   end
+
+  -- override global print to notify
+  _G.print = function(...)
+    local print_safe_args = {}
+    local _ = {...}
+    for i=1, #_ do
+        table.insert(print_safe_args, tostring(_[i]))
+    end
+    local message = table.concat(print_safe_args, ' ')
+    vim.notify(message, vim.log.levels.INFO)
+  end
 end
 
 return M
