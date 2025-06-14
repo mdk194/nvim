4eb9e9ec316be1418f4a54feadd5454934889531
Khoa Mai <mdk194@gmail.com>
2025-06-14 09:56:05 +0300

    gopher: update

--- lua/plugins/gopher.lua
+++ lua/plugins/gopher.lua
@@ -1,17 +1,44 @@
 local M = {
   "olexsmir/gopher.nvim",
-  cmd = { "GoTagAdd", "GoTagRm", "GoTestAdd", "GoTestsAll", "GoTestsExp", "GoGenerate", "GoGet", "GoMod", "GoImpl", "GoCmt", "GoIfErr" },
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
-  },
+  ft = "go",
   build = function()
     vim.cmd.GoInstallDeps()
   end,
 }
 
 function M.config()
-  require("gopher").setup()
+  require("gopher").setup({
+    -- log level, you might consider using DEBUG or TRACE for debugging the plugin
+    log_level = vim.log.levels.INFO,
+
+    -- timeout for running internal commands
+    timeout = 2000,
+
+    commands = {
+      go = "go",
+      gomodifytags = "gomodifytags",
+      gotests = "gotests",
+      impl = "impl",
+      iferr = "iferr",
+    },
+    gotests = {
+      -- gotests doesn't have template named "default" so this plugin uses "default" to set the default template
+      template = "default",
+      -- path to a directory containing custom test code templates
+      template_dir = nil,
+      -- switch table tests from using slice to map (with test name for the key)
+      named = false,
+    },
+    gotag = {
+      transform = "snakecase",
+      -- default tags to add to struct fields
+      default_tag = "json",
+    },
+    iferr = {
+      -- choose a custom error message
+      message = nil,
+    },
+  })
 end
 
 return M
