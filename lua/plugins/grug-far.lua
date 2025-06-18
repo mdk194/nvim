6d8ca2f7045948ef12b450ed3c73dfd15e3d8386
Khoa Mai <mdk194@gmail.com>
2025-06-19 01:19:22 +0300

    add grug-far

---
+++ lua/plugins/grug-far.lua
@@ -0,0 +1,13 @@
+return {
+  'MagicDuck/grug-far.nvim',
+  -- Note (lazy loading): grug-far.lua defers all it's requires so it's lazy by default
+  -- additional lazy config to defer loading is not really needed...
+  keys = {
+    { '<leader>sr', function() require('grug-far').open() end },
+  },
+  config = function()
+    require('grug-far').setup({
+      windowCreationCommand = 'vsplit',
+    });
+  end
+}
