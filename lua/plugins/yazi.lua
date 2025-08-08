79c0518630416c149b79a0f46999e7a8c2ce4cb5
Khoa Mai <mdk194@gmail.com>
2025-08-08 22:46:53 +0300

    add yazi.nvim

---
+++ lua/plugins/yazi.lua
@@ -0,0 +1,39 @@
+return {
+  'mikavilpas/yazi.nvim',
+  event = "VeryLazy",
+  dependencies = {
+    { "nvim-lua/plenary.nvim", lazy = true },
+  },
+  keys = {
+    {
+      -- Open in the current working directory
+      "<c-y>",
+      "<cmd>Yazi cwd<cr>",
+      desc = "Open the file manager in nvim's working directory",
+    },
+    {
+      "<leader>y",
+      "<cmd>Yazi toggle<cr>",
+      desc = "Resume the last yazi session",
+    },
+  },
+  opts = {
+    -- if you want to open yazi instead of netrw, see below for more info
+    open_for_directories = false,
+    keymaps = {
+      show_help = "<f1>",
+      open_file_in_vertical_split = "<c-v>",
+      open_file_in_horizontal_split = "<c-x>",
+      open_file_in_tab = "<c-t>",
+      grep_in_directory = "<c-s>",
+      replace_in_directory = "<c-g>",
+      cycle_open_buffers = "<tab>",
+      copy_relative_path_to_selected_files = "<c-y>",
+      send_to_quickfix_list = "<c-q>",
+      change_working_directory = "<c-\\>",
+      open_and_pick_window = "<c-o>",
+    },
+    floating_window_scaling_factor = 0.9,
+  },
+}
+
