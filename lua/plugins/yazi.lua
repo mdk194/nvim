66af3ae9ccc208dce7d2186f0a95fa9ed93b4d05
Khoa Mai <mdk194@gmail.com>
2025-08-08 23:01:52 +0300

    yazi: use snack for grep

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -21,12 +21,12 @@ return {
     -- if you want to open yazi instead of netrw, see below for more info
     open_for_directories = false,
     keymaps = {
-      show_help = "<f1>",
+      show_help = "~",
       open_file_in_vertical_split = "<c-v>",
       open_file_in_horizontal_split = "<c-x>",
       open_file_in_tab = "<c-t>",
-      grep_in_directory = "<c-s>",
-      replace_in_directory = "<c-g>",
+      grep_in_directory = "<c-g>",
+      replace_in_directory = "<c-r>",
       cycle_open_buffers = "<tab>",
       copy_relative_path_to_selected_files = "<c-y>",
       send_to_quickfix_list = "<c-q>",
@@ -34,6 +34,9 @@ return {
       open_and_pick_window = "<c-o>",
     },
     floating_window_scaling_factor = 0.9,
+    integrations = {
+      grep_in_directory = "snacks.picker",
+    },
   },
 }
 
