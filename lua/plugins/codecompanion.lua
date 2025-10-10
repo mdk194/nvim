f57f01c7527dc23f2ffa01f4d5856939e1b66f44
Khoa Mai <mdk194@gmail.com>
2025-10-11 01:36:03 +0300

    enable codecompanion by default

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -1,8 +1,10 @@
 local M = {
   "olimorris/codecompanion.nvim",
+  lazy = false,
   cmd = { "CodeCompanionChat", "CodeCompanionActions" },
   keys = {
     { '<leader>i', [[<cmd>lua require('codecompanion').chat()<CR>]] },
+    { '<leader>a', [[<cmd>lua require('codecompanion').actions()<CR>]] },
   },
   dependencies = {
     "nvim-lua/plenary.nvim",
@@ -12,6 +14,22 @@ local M = {
 
 function M.config()
   require("codecompanion").setup({
+    display = {
+      chat = {
+        intro_message = "✨! Press ? for options",
+        icons = {
+          chat_context = "📎️", -- You can also apply an icon to the fold
+          chat_fold = " ",
+        },
+        auto_scroll = false,
+        fold_context = true,
+        fold_reasoning = true,
+        show_settings = false,
+      },
+      inline = {
+        layout = "vertical", -- vertical|horizontal|buffer
+      },
+    },
     strategies = {
       chat = {
         adapter = "copilot",
@@ -41,6 +59,17 @@ function M.config()
       },
       inline = {
         adapter = "copilot",
+        keymaps = {
+          accept_change = {
+            modes = { n = "ga" },
+            description = "Accept the suggested change",
+          },
+          reject_change = {
+            modes = { n = "gr" },
+            opts = { nowait = true },
+            description = "Reject the suggested change",
+          },
+        },
       },
     },
     adapters = {
@@ -58,6 +87,46 @@ function M.config()
       },
     },
   })
+
+  local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+  local group = vim.api.nvim_create_augroup("CodeCompanionFidgetHooks", { clear = true })
+  vim.api.nvim_create_autocmd({ "User" }, {
+    pattern = "CodeCompanion*",
+    group = group,
+    callback = function(request)
+      if request.match == "CodeCompanionChatSubmitted" then
+        return
+      end
+
+      local msg
+
+      -- msg = "[CodeCompanion] " .. request.match:gsub("CodeCompanion", "")
+      msg = request.match:gsub("CodeCompanion", "")
+
+      vim.notify(msg, "info", {
+        timeout = 1000,
+        keep = function()
+          return vim.iter({ "InlineStarted" }):fold(
+            false,
+            function(acc, cond)
+              return acc or vim.endswith(request.match, cond)
+            end)
+        end,
+        id = "code_companion_status",
+        title = "CodeCompanion",
+        opts = function(notif)
+          notif.icon = ""
+          if vim.endswith(request.match, "Started") then
+            ---@diagnostic disable-next-line: undefined-field
+            notif.icon = spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+          elseif vim.endswith(request.match, "Finished") then
+            notif.icon = " "
+          end
+        end,
+      })
+    end,
+  })
+
 end
 
 return M
